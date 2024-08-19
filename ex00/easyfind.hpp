edd4d800270e000d196cff2c6d426a27ca416bbb
hachi868 <24986912+hachi868@users.noreply.github.com>
2024-08-19 12:52:20 +0900

    [update]test, output message

--- ex00/easyfind.hpp
+++ ex00/easyfind.hpp
@@ -5,17 +5,22 @@
 #include <list>
 #include <algorithm>
 
+#define MSG "\033[34m"
+#define ERROR "\033[31m"
+#define RESET "\033[0m"
+
 template <typename T>void easyfind(const T &container, int n) {
 	typename T::const_iterator it = std::find(container.begin(), container.end(), n);
 
-	if (it != container.end())
-		std::cout << "FIND!" << std::endl;
-	else
-		std::cerr << "404" << std::endl;
-	return ;
+	if (it != container.end()) {
+        int index = std::distance(container.begin(), it);
+        std::cout << MSG << "[FOUND] " << n << " is found at position " << index << RESET << std::endl;
+    } else {
+		std::cerr << ERROR << "[NOT FOUND] " << n << " is not found in container" << RESET << std::endl;
+    }
 }
 
 //is_same_v, is_integral_vはc++98で使えない
-//std::findはC11以降なので、STLコンテナのfindメンバ関数を使用。
+//std::vector<int>::iterator result：auto はc++11以降
 
 #endif //EASYFIND_HPP
