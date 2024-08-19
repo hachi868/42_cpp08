7622d8caaf03c87bf9832144c012da9d6df7b590
hachi868 <24986912+hachi868@users.noreply.github.com>
2024-08-19 14:34:21 +0900

    [add]ex01

--- ex01/Span.cpp
+++ ex01/Span.cpp
@@ -1,1 +1,64 @@
 #include "Span.hpp"
+
+const std::string Span::RESET = "\033[0m";
+const std::string Span::DEBUG = "\033[90m";
+const std::string Span::STATE = "\033[36m";
+const std::string Span::ALERT = "\033[31m";
+const std::string Span::MSG = "\033[34m";
+
+Span::Span(unsigned int N) : N_(N), container_(N)
+{
+    std::cout << Span::DEBUG << "[Span] constructor called (N)" << Span::RESET << std::endl;
+}
+
+Span::Span(const Span &obj) : N_(obj.N_), container_(obj.container_)
+{
+    std::cout << Span::DEBUG << "[Span] copy constructor called" << Span::RESET << std::endl;
+}
+
+Span &Span::operator = (const Span &obj)
+{
+    std::cout << Span::DEBUG << "[Span] assignation operator called" << Span::RESET << std::endl;
+    if (this != &obj) {
+        this->N_ = obj.N_;
+        this->container_ = obj.container_;
+    }
+    return (*this);
+}
+
+Span::~Span()
+{
+    std::cout << Span::DEBUG << "[Span] destructor called" << Span::RESET << std::endl;
+}
+
+void	Span::addNumber(int a)
+{
+    if (container_.size() >= N_)
+        throw fullAlready();
+    container_.push_back(a);
+}
+
+size_t Span::shortestSpan(void) const
+{
+    return 0;
+}
+
+size_t Span::longestSpan(void) const
+{
+    return 0;
+}
+
+void Span::fillRange(int a, int b)
+{
+
+}
+
+const char* Span::fullAlready::what() const throw()
+{
+    return ("container is full already.");
+}
+
+const char* Span::InvalidSpan::what() const throw()
+{
+    return ("No span can be found. The collection is either empty or has only one element.");
+}
--- ex01/Span.hpp
+++ ex01/Span.hpp
@@ -1,9 +1,44 @@
 #ifndef SPAN_HPP
 #define SPAN_HPP
 
+#include <stdio.h>
+#include <iostream>
+//#include <list>
+//#include <algorithm>
+#include <vector>
 
 class Span {
+public:
+    Span(unsigned int N);
+    Span(const Span &obj);
+    Span &operator = (const Span &obj);
+    ~Span();
 
+    void addNumber(int a);
+    size_t shortestSpan(void) const;
+    size_t longestSpan(void) const;
+    void fillRange(int a, int b);
+
+    const static std::string RESET;
+    const static std::string DEBUG;
+    const static std::string STATE;
+    const static std::string ALERT;
+    const static std::string MSG;
+
+    //throw
+    class fullAlready : public std::exception {
+    public:
+        virtual const char* what() const throw();
+    };
+
+    class InvalidSpan : public std::exception {
+    public:
+        virtual const char* what() const throw();
+    };
+private:
+    unsigned int N_;
+    std::vector<int> container_;
+    Span();
 };
 
 
